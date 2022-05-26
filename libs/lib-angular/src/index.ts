c60885747ae407ff876f17e0cfac2af02a283eb3
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-05-26 15:48:53 +0200

    update exports of lib-angular

--- libs/lib-angular/src/index.ts
+++ libs/lib-angular/src/index.ts
@@ -1,8 +1,11 @@
-export * from './lib/components/app-navigation-container/app-navigation-container.module';
+export * from './lib/components/app-navigation-container/app-navigation-container-desktop.component';
+export * from './lib/components/app-navigation-container/app-navigation-container-mobile.component';
+export * from './lib/components/app-navigation-container/app-navigation-container.component';
 export * from './lib/components/app-navigation-container/app-navigation-container.module';
 export * from './lib/components/blockablediv/blockablediv.module';
-
+export * from './lib/errors/application.error';
 export * from './lib/models/navigation-item.model';
 export * from './lib/models/theme.model';
-
 export * from './lib/pipes/duration.pipe';
+export * from './lib/services/responsive.service';
+export * from './lib/services/theme.service';
