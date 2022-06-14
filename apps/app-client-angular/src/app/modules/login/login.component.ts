43ec5826c09af8163b31f12cc893a730b70afdb1
EstevanGAY <estevan.gay@gmail.com>
2022-06-14 13:50:59 +0200

    Fix login #8

--- apps/app-client-angular/src/app/modules/login/login.component.ts
+++ apps/app-client-angular/src/app/modules/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { appRoutes } from '@libraries/lib-scrum-toolbox';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
@@ -7,4 +9,31 @@ import { appRoutes } from '@libraries/lib-scrum-toolbox';
 })
 export class LoginComponent {
   routes = appRoutes;
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+  
+  constructor(
+    // private readonly signinService: SigninService,
+    private readonly fb: FormBuilder,
+    public readonly router: Router
+  ) {}
+
+
+  async submitForm() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    /* try {
+      await this.signinService.signin({
+        email: this.form.get('email')?.value,
+        password: this.form.get('password')?.value,
+      });
+    } catch (error) {
+      throw new ImpossibleToSigninError();
+    }*/
+  } 
 }
