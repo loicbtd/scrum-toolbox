ad6356cfd28b929d69f229fda68b2cbb09187526
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-06-27 15:49:49 +0200

    remove old imports

--- apps/app-client-electron/src/app/datasources/main.datasource.ts
+++ apps/app-client-electron/src/app/datasources/main.datasource.ts
@@ -2,7 +2,6 @@ import { homedir } from 'os';
 import { join } from 'path';
 import { ConnectionOptions } from 'typeorm';
 import { environment } from '../../environments/environment';
-import { InitializeDatabase1 } from '../migrations/1-initialize-database';
 import {
   ProjectEntity,
   SprintEntity,
@@ -14,12 +13,6 @@ import {
   SprintMemberEntity,
   ProjectMemberEntity,
 } from '@libraries/lib-scrum-toolbox';
-import { AddTaskCapacity2 } from '../migrations/2-add-task-capacity';
-import { AddColosToSprintStatus3 } from '../migrations/3-add-colors-sprint-status';
-import { UpgradeToNewSchema4 } from '../migrations/4-UpgradeToNewSchema';
-import { RenameUserTable5 } from '../migrations/5-RenameUserTable';
-import { UpdateEntities6 } from '../migrations/6-UpdateEntities';
-import { UpdateTaskEntity7 } from '../migrations/7-UpdateTaskEntity';
 import { InitializeDatabase1656337596981 } from '../migrations/1656337596981-InitializeDatabase';
 
 export const mainDataSource = {
